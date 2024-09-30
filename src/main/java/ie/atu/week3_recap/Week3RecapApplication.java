dd7633c102d4c9c460bd84aa12b5ee742aef5562
G00402034 <G00402034@atu.ie>
2024-09-30 16:13:04 +0100

    crated new project Week3ReCap

---
+++ src/main/java/ie/atu/week3_recap/Week3RecapApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3_recap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3RecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3RecapApplication.class, args);
+    }
+
+}
